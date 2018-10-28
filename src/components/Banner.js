7b1e2426eee6d6b17055de6add5894722cae2ba5
vollervidez <vollervi@outlook.com>
2018-10-28 11:01:03 -0500

    Changed text on homepage banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,11 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Hi, my name is Victor</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>I am a Front-End Developer<br />
+                based out of Austin, TX.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
