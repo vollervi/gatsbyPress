a43c89ec275ba7208119058e0621d29e0b617f2e
vollervidez <vollervi@outlook.com>
2018-10-29 09:44:36 -0500

    added new component and scss file

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = (props) => (
+    <section id="about">
+        <div className="inner">
+            <header className="major">
+                <h2>Who Am I?</h2>
+            </header>
+            <div className="content">
+                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                <br />
+                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default About
