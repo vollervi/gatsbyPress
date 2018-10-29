c6fa6849aad2a12d171bd3f94a507ed1d02e4b5a
vollervidez <vollervi@outlook.com>
2018-10-29 09:45:56 -0500

    added hero image and intro text to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,16 +3,22 @@ import React from 'react'
 const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
-            <header className="major">
+            <header className="major style2">
                 <h1>Hi, my name is Victor</h1>
             </header>
             <div className="content">
                 <p>I am a Front-End Developer<br />
                 based out of Austin, TX.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
             </div>
+            <ul className="icons">
+                <li><a href="https://github.com/vollervidez" target="_blank" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="https://github.com/vollervidez" target="_blank" className="icon alt fa-codepen"><span className="label">GitHub</span></a></li>
+                <li><a href="https://twitter.com/vollervidez" target="_blank" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="https://www.linkedin.com/in/victor-ollervidez/" target="_blank" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+            </ul>
+            <ul className="actions">
+                    <li><a href="#" className="button next scrolly">Get In Touch</a></li>
+            </ul>
         </div>
     </section>
 )
