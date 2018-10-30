26a6fa433852510b9bacdd7e5ed601b6eded2ac5
vollervidez <vollervi@outlook.com>
2018-10-30 17:51:36 -0500

    added portfolio grid to container

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 import Banner from '../components/Banner'
 import About from '../components/About'
 
-import pic01 from '../assets/images/pic01.jpg'
+import pic01 from '../assets/images/wa-website.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
@@ -30,62 +30,67 @@ class HomeIndex extends React.Component {
                 <About />
 
                 <div id="main">
-                    <section id="two" className="tiles">
-                        <article style={{backgroundImage: `url(${pic01})`}}>
-                            <header className="major">
-                                <h3>Warm Audio LLc.</h3>
-                                <p>Web Development & Web Design</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic02})`}}>
-                            <header className="major">
-                                <h3>City of Pharr Texas</h3>
-                                <p>Web Development & Web Design</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic03})`}}>
-                            <header className="major">
-                                <h3>City of Pharr 2017 Park Improvement Plan</h3>
-                                <p>Web Development & Web Design</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic04})`}}>
-                            <header className="major">
-                                <h3>Pharr International Bridge</h3>
-                                <p>Web Development & Web Design</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic05})`}}>
-                            <header className="major">
-                                <h3>Rio Grande Guardian</h3>
-                                <p>Web Development</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic06})`}}>
-                            <header className="major">
-                                <h3>Etiam</h3>
-                                <p>Feugiat amet tempus</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                    </section>
-                    <section id="one">
-                        <div className="inner">
-                            <header className="major">
-                                <h2>Massa libero</h2>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/landing" className="button next">Get Started</Link></li>
-                            </ul>
-                        </div>
-                    </section>
+                    <div className="inner">
+                        <header className="major">
+                            <h2>Some Recent Work</h2>
+                        </header>
                     
+                        <section id="one" className="tiles">
+                            <article style={{backgroundImage: `url(${pic01})`}}>
+                                <header className="major">
+                                    <h3>Warm Audio LLc.</h3>
+                                    <p>Web Development & Web Design</p>
+                                </header>
+                                <Link to="/landing" className="link primary"></Link>
+                            </article>
+                            <article style={{backgroundImage: `url(${pic02})`}}>
+                                <header className="major">
+                                    <h3>City of Pharr Texas</h3>
+                                    <p>Web Development & Web Design</p>
+                                </header>
+                                <Link to="/landing" className="link primary"></Link>
+                            </article>
+                            <article style={{backgroundImage: `url(${pic03})`}}>
+                                <header className="major">
+                                    <h3>City of Pharr 2017 Park Improvement Plan</h3>
+                                    <p>Web Development & Web Design</p>
+                                </header>
+                                <Link to="/landing" className="link primary"></Link>
+                            </article>
+                            <article style={{backgroundImage: `url(${pic04})`}}>
+                                <header className="major">
+                                    <h3>Pharr International Bridge</h3>
+                                    <p>Web Development & Web Design</p>
+                                </header>
+                                <Link to="/landing" className="link primary"></Link>
+                            </article>
+                            <article style={{backgroundImage: `url(${pic05})`}}>
+                                <header className="major">
+                                    <h3>Rio Grande Guardian</h3>
+                                    <p>Web Development</p>
+                                </header>
+                                <Link to="/landing" className="link primary"></Link>
+                            </article>
+                            <article style={{backgroundImage: `url(${pic06})`}}>
+                                <header className="major">
+                                    <h3>The F Word Gift Shop</h3>
+                                    <p>Web Development</p>
+                                </header>
+                                <Link to="/landing" className="link primary"></Link>
+                            </article>
+                        </section>
+                        {/* <section id="two">
+                            <div className="inner">
+                                <header className="major">
+                                    <h2>Massa libero</h2>
+                                </header>
+                                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                                <ul className="actions">
+                                    <li><Link to="/landing" className="button next">Get Started</Link></li>
+                                </ul>
+                            </div>
+                        </section> */}
+                    </div> 
                 </div>
 
             </Layout>
