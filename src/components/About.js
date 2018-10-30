7522eb3ba7243a4a452e61c05c28ad7ca206782a
vollervidez <vollervi@outlook.com>
2018-10-30 17:48:34 -0500

    added about me text

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,8 @@ const About = (props) => (
                 <h2>About Me</h2>
             </header>
             <div className="content">
-                <p>Originally form South Texas but now residing in Austin, TX  </p>
-                <br />
-                <p>For over 3 years, I have been building websites using the WordPress CMS and various other tools for a wide array of clients from small newspaper service and medium sized business to large local government. Throughout this time, I have served as the lead developer (and designer) for all my projects.</p>
+                <p>Hey! I’m just a regular dude that likes building good looking and functional websites that are easy to use. I also enjoy the occasional hike around one of the local Austin, Tx green belts or a cold pint at a local brewery.</p>
+                <p>For over 3 years I have been working as a web developer in professional fast paced environments helping companies both small and large get their brand and identity across the internet to their users. My professional career has been mainly focused around building websites around the WordPress CMS to allow clients to easily take over the site at any point after development was completed.</p>
             </div>
         </div>
     </section>
